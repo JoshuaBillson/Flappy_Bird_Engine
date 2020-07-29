34907c17090553f318ee50c634ecc188dd0f337b
Joshua Billson <jmbillson@outlook.com>
2020-07-29 04:33:57 -0600

    Updated Dependencies

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="flappy_bird_engine", 
+    install_requires=["pygame==1.9.6", "wheel==0.34.2"],
+    name="flappy_bird_engine",
     version="0.0.1",
     author="Joshua Billson",
     author_email="jmbillson@outlook.com",
     description="A game engine for Flappy Bird powered by Pygame.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/JoshuaBillson/Flappy_Bird_Engine.git", 
-    package_dir={'': 'flappy_bird'},
-    packages=find_packages('flappy_bird'),
+    url="https://github.com/JoshuaBillson/Flappy_Bird_Engine.git",
+    package_dir={"": "flappy_bird"},
+    packages=find_packages("flappy_bird"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
